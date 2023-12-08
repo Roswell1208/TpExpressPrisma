b47cc1b2a07739abf6cc767d7b76a3b894fd2fed
Hugo Poquet <hugo.poquet@icloud.com>
2023-12-08 01:30:40 +0100

    <dev>(Back): Middlewares

--- build/index.js
+++ build/index.js
@@ -5,19 +5,27 @@ var __importDefault = (this && this.__importDefault) || function (mod) {
 Object.defineProperty(exports, "__esModule", { value: true });
 const express_1 = __importDefault(require("express"));
 const body_parser_1 = __importDefault(require("body-parser"));
-const passport_config_1 = require("./config/passport-config");
+const authMiddleware_1 = require("./middlewares/authMiddleware");
 const productsRoutes_1 = __importDefault(require("./routes/productsRoutes"));
 const usersRoutes_1 = __importDefault(require("./routes/usersRoutes"));
 const ordersRoutes_1 = __importDefault(require("./routes/ordersRoutes"));
+const helmet_1 = __importDefault(require("helmet"));
+const cors_1 = __importDefault(require("cors"));
+const express_validator_1 = require("express-validator");
+const expressValidatorMiddleware_1 = require("./middlewares/expressValidatorMiddleware");
 const app = (0, express_1.default)();
 const port = process.env.PORT || 3000;
+// Middleware pour sécuriser l'application
+app.use((0, helmet_1.default)());
+// Middleware pour autoriser les requêtes depuis n'importe quelle origine
+app.use((0, cors_1.default)());
 // Middleware pour parser le corps des requêtes en JSON
 app.use(body_parser_1.default.json());
 // Middleware Passport
-app.use(passport_config_1.passportMiddleware);
+app.use(authMiddleware_1.passportMiddleware);
 app.use('/products', productsRoutes_1.default);
 app.use('/users', usersRoutes_1.default);
-app.use('/orders', passport_config_1.requireAuth, ordersRoutes_1.default);
+app.use('/orders', authMiddleware_1.requireAuth, (0, express_validator_1.body)('email').isEmail().normalizeEmail(), (0, express_validator_1.body)('password').isLength({ min: 5 }), expressValidatorMiddleware_1.handleValidationErrors, ordersRoutes_1.default);
 app.listen(port, () => {
     console.log(`Server is running at http://localhost:${port}`);
 });
--- src/index.ts
+++ src/index.ts
@@ -1,12 +1,22 @@
 import express from 'express';
 import bodyParser from 'body-parser';
-import { passportMiddleware, requireAuth } from './config/passport-config';
+import { passportMiddleware, requireAuth } from './middlewares/authMiddleware';
 import productsRouter from './routes/productsRoutes';
 import usersRouter from './routes/usersRoutes';
 import ordersRouter from './routes/ordersRoutes';
+import helmet from 'helmet';
+import cors from 'cors';
+import { body } from 'express-validator';
+import { handleValidationErrors } from './middlewares/expressValidatorMiddleware';
 
 const app = express();
 const port = process.env.PORT || 3000;
+
+// Middleware pour sécuriser l'application
+app.use(helmet());
+
+// Middleware pour autoriser les requêtes depuis n'importe quelle origine
+app.use(cors());
 
 // Middleware pour parser le corps des requêtes en JSON
 app.use(bodyParser.json());
@@ -16,7 +26,7 @@ app.use(passportMiddleware);
 
 app.use('/products', productsRouter);
 app.use('/users', usersRouter);
-app.use('/orders', requireAuth, ordersRouter);
+app.use('/orders', requireAuth, body('email').isEmail().normalizeEmail(), body('password').isLength({ min: 5 }), handleValidationErrors, ordersRouter);
 
 app.listen(port, () => {
   console.log(`Server is running at http://localhost:${port}`);
